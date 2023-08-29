511616105998a0afaa7696571b136cfdd94bcfca
NotAstronot <feririski409@gmail.com>
2023-08-29 19:08:14 +0700

    update

---
+++ PHP Object Oriented Programming (OOP)/PHP 8/NamedArgument.php
@@ -0,0 +1,10 @@
+<?php
+
+
+function sayHello(string $first, string $middle = "", string $last): void
+{
+    echo "Hello $first $middle $last" . PHP_EOL;
+}
+
+sayHello("Not", "atronot", "Boy"):
+sayHello()
