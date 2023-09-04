f2384383f86e6ceb4ce2eba40876accb50ca741c
NotAstronot <feririski409@gmail.com>
2023-09-04 20:21:30 +0700

    update

---
+++ PHP 8/NewStringFunction.php
@@ -0,0 +1,15 @@
+<?php
+
+
+var_dump(str_contains("Not Astronot", "Boys"));
+var_dump(str_contains("Not Astronot", "Astronot"));
+var_dump(str_contains("Not Astronot", "salah"));
+
+var_dump(str_starts_with("Not Astronot", "Not"));
+var_dump(str_starts_with("Not Astronot", "Boys"));
+var_dump(str_starts_with("Not Astronot", "Boys"));
+
+
+var_dump(str_ends_with("Not Astronot", "Not"));
+var_dump(str_ends_with("Not Astronot", "Not"));
+var_dump(str_ends_with("Not Astronot", "Not"));
